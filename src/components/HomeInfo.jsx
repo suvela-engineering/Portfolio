db7e5a54243f3426aa42a2ac950dea38052f9b4d
Juho Suvela <juho.suvela@suvela-engineering.fi>
2024-02-17 20:30:24 +0200

    combine contact info

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {arrow} from "../assets/icons/";
+import { arrow } from "../assets/icons/";
 
 const InfoBox = ({ text, link, btnText }) => {
   <div className='info-box'>
@@ -13,24 +13,37 @@ const InfoBox = ({ text, link, btnText }) => {
 
 const renderContent = {
   1: (
-    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-      Hi, I'm <span className='font-semibold'>Juho</span>👋
-      <br />
-      A Full-Stack Software Developer from Finland.
-    </h1>
-  ),
-  2: (
-    <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Worked with many companies <br /> and picked up many skills along the way
-      </p>
+    <div >
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm <span className='font-semibold'>Juho</span>👋
+        <br />
+        A Full-Stack Software Developer from Finland.
+      </h1>
 
-      <Link to='/about' className='neo-brutalism-white neo-btn'>
-        Learn more
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
+        </p>
+
+        <Link to='/contact' className='neo-brutalism-white neo-btn'>
+          Let's talk
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
     </div>
   ),
+  // 2: (
+  //   <div className='info-box'>
+  //     <p className='font-medium sm:text-xl text-center'>
+  //       Worked with many companies <br /> and picked up many skills along the way
+  //     </p>
+
+  //     <Link to='/about' className='neo-brutalism-white neo-btn'>
+  //       Learn more
+  //       <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+  //     </Link>
+  //   </div>
+  // ),
   // 3: (
   //   <div className='info-box'>
   //     <p className='font-medium text-center sm:text-xl'>
@@ -43,18 +56,18 @@ const renderContent = {
   //     </Link>
   //   </div>
   // ),
-  4: (
-    <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
-      </p>
+  // 4: (
+  //   <div className='info-box'>
+  //     <p className='font-medium sm:text-xl text-center'>
+  //       Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
+  //     </p>
 
-      <Link to='/contact' className='neo-brutalism-white neo-btn'>
-        Let's talk
-        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
-      </Link>
-    </div>
-  )
+  //     <Link to='/contact' className='neo-brutalism-white neo-btn'>
+  //       Let's talk
+  //       <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+  //     </Link>
+  //   </div>
+  // )
 }
 
 const HomeInfo = ({ currentStage }) => {
